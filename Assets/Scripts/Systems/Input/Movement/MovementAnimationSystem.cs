2193711e3295959479c93294c87d247209744d5e
Shadow-Dev <Lite5h4dow@users.noreply.github.com>
2019-04-22 13:46:50 +0100

    Small changes to Variables
    Increased Movemnet speed for debugging.
    
    Started animation Integration

---
+++ Assets/Scripts/Systems/Input/Movement/MovementAnimationSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace RogueGo {
+  public class MovementAnimationSystem : ComponentSystem {
+    EntityQuery player;
+
+    protected override void OnCreateManager () {
+      player = GetEntityQuery (
+        typeof (Player),
+        typeof (Animator),
+        typeof (MovementInput)
+      );
+    }
+
+    protected override void OnUpdate () {
+      
+    }
+  }
+}
--- Assets/Scripts/Systems/Test Systems/MoveInputDebugSystem.cs
+++ Assets/Scripts/Systems/Test Systems/MoveInputDebugSystem.cs
@@ -1,24 +1,24 @@
-﻿using Unity.Collections;
-using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
-using UnityEngine;
+﻿// using Unity.Collections;
+// using Unity.Entities;
+// using Unity.Mathematics;
+// using Unity.Transforms;
+// using UnityEngine;
 
-namespace RogueGo {
-  public class MoveInputDebugSystem : ComponentSystem {
-    EntityQuery player;
+// namespace RogueGo {
+//   public class MoveInputDebugSystem : ComponentSystem {
+//     EntityQuery player;
 
-    protected override void OnCreateManager () {
-      player = GetEntityQuery (
-        typeof (Player),
-        typeof (MovementInput)
-      );
-    }
+//     protected override void OnCreateManager () {
+//       player = GetEntityQuery (
+//         typeof (Player),
+//         typeof (MovementInput)
+//       );
+//     }
 
-    protected override void OnUpdate () {
-      Entities.With (player).ForEach ((ref MovementInput movement) => {
-        Debug.Log(movement.Value);
-      });
-    }
-  }
-}
+//     protected override void OnUpdate () {
+//       Entities.With (player).ForEach ((ref MovementInput movement) => {
+//         Debug.Log(movement.Value);
+//       });
+//     }
+//   }
+// }
